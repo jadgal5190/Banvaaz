cca4eaa1d07a9fb55256af6075ba4ae0ec571120
jadgal5190 <83171173+jadgal5190@users.noreply.github.com>
2021-12-11 19:52:19 +0330

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".header");
+toggler.addEventListener("click", (e) => {
+    navbar.classList.toggle("nav__expanded");
+});
